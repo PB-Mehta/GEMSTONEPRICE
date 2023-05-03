7f341a7b97b751bd22504091dbb6248f65d6ecc5
Your Name <m3hta08@gmail.com>
2023-05-03 14:02:52 +0530

    Application commit

--- src/utils.py
+++ src/utils.py
@@ -33,8 +33,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             # Predict Testing data
             y_test_pred =model.predict(X_test)
 
-            # Get R2 scores for train and test data
-            #train_model_score = r2_score(ytrain,y_train_pred)
+            
             test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] =  test_model_score
@@ -43,4 +42,12 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
         raise CustomException(e,sys)
